38e00203660eb5a89f182114b21a66f708e2424b
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2025-01-02 14:46:38 -0300

    Adding abstract class Account

---
+++ src/main/java/org/example/Account.java
@@ -0,0 +1,52 @@
+package org.example;
+
+public abstract class Account {
+
+    private String name;
+
+    private long number;
+
+    private double balance;
+
+    public Account(String name, long number, double balance) {
+        this.name = name;
+        this.number = number;
+        this.balance = balance;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public long getNumber() {
+        return this.number;
+    }
+
+    public double getBalance(){
+        return this.balance;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public void setNumber(long number){
+        this.number = number;
+    }
+
+    public void setBalance(double balance){
+        this.balance = balance;
+    }
+
+    public void Deposit(double value){
+        this.balance = this.balance + value;
+    }
+
+    public void Withdraw(double value){
+        this.balance = this.balance - value;
+    }
+
+    public void showInfo(){
+        System.out.println("Name: " + name + ", Account Number: " + number + ", Balance: " +balance);
+    }
+}
