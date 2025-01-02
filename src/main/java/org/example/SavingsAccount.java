e4a061f595807a46561ee79b0a8e3e53230439d8
Rodrigo Silva <rodrigo.92.srb@gmail.com>
2025-01-02 17:15:05 -0300

    Starting a withdraw's control rules

--- src/main/java/org/example/SavingsAccount.java
+++ src/main/java/org/example/SavingsAccount.java
@@ -4,6 +4,8 @@ public class SavingsAccount extends Account{
 
     private int daysAfterLastWithdraw;
 
+    private int availableWithdraws;
+
     public SavingsAccount(String name, long number, double balance) {
         super(name, number, balance);
     }
@@ -14,5 +16,35 @@ public class SavingsAccount extends Account{
 
     public void setDaysAfterLastWithdraw(int days){
         this.daysAfterLastWithdraw = days;
+    }
+
+    public int getAvailableWithdraws(){
+        return this.availableWithdraws;
+    }
+
+    public void setAvailableWithdraws(int withdraws){
+        this.availableWithdraws = this.availableWithdraws + withdraws;
+    }
+
+    public void useWithdraw(){
+        this.availableWithdraws = this.availableWithdraws - 1;
+    }
+
+    @Override
+    public void showInfo(){
+        System.out.println("Account Info\nName: " + this.getName() + ", Account Number: " + this.getNumber() + ", " +
+                "Balance: " +this.getBalance() + ", Available Withdraws: " + this.getAvailableWithdraws());
+    }
+
+    @Override
+    public void Withdraw(double value){
+        if(this.availableWithdraws > 0){
+            this.useWithdraw();
+            this.setBalance(this.getBalance() - value);
+            this.showInfo();
+        }
+        else{
+            System.out.println("You don't have available withdraws!");
+        }
     }
 }
